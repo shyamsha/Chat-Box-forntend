33af7c4658b0e7ddbe8e9923de5f4c3e7c92260e
shyamsha <shyamkumar97@live.com>
2019-05-21 19:39:52 +0530

    authentication finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Register from "./components/users/Register";
+import Login from "./components/users/Login";
+import Logout from "./components/users/Logout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//login: false,
+			//logout: false
+		};
+	}
+	handleLogin = () => {
+		this.setState(() => ({
+			// login: true,
+			// logout: false
+		}));
+	};
+	handleLogout = () => {
+		this.setState(() => ({
+			// logout: true,
+			// login: false
+		}));
+	};
+	render() {
+		let login = false;
+		let logout = false;
+
+		if (localStorage.getItem("token")) {
+			login = true;
+		} else {
+			logout = true;
+		}
+		return (
+			<BrowserRouter>
+				<div style={{ color: "white", textDecoration: "none" }}>
+					{logout && (
+						<div>
+							<Link to="/user/register">Register</Link>
+							<Link to="/user/login">Login</Link>
+						</div>
+					)}
+					{login && (
+						<div>
+							<Link to="/user/logout">Logout</Link>
+						</div>
+					)}
+				</div>
+				<div>
+					<Switch>
+						<Route path="/user/register" component={Register} exact />
+						<Route
+							path="/user/login"
+							render={props => {
+								return (
+									<Login {...props} handleLogin={this.handleLogin} exact />
+								);
+							}}
+							exact
+						/>
+						<Route
+							path="/user/logout"
+							render={props => {
+								return (
+									<Logout {...props} handleLogout={this.handleLogout} exact />
+								);
+							}}
+							exact
+						/>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
